85a13c14969a5510bd82f25453c9d0cbac850ef1
Adari Vamsi <adarivamsi@gmail.com>
2020-04-04 23:04:47 -0400

    AV Logo Application

---
+++ AVLogo/app/src/main/java/com/avvisualefx/avlogo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.avvisualefx.avlogo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adari on 7/18/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
